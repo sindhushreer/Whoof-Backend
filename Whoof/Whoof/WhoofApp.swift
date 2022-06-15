aa9ef827a806cf3eee4121550983f1b046754973
Suchith Nayaka <Suchithnayaka>
2022-06-15 23:16:33 +0530

    Firebase not working

--- Whoof/Whoof/WhoofApp.swift
+++ Whoof/Whoof/WhoofApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+content_copy
+
+    return true
+  }
+}
 
 @main
 struct WhoofApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var userAuth = UserAuth()
     var body: some Scene {
         WindowGroup {
